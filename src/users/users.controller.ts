23db4e0f617d30379553f383ecc8a567762a56d9
Abdelhameed Ismail <3b7ameed@gmail.com>
2021-12-17 23:29:06 +0200

    Sinup service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createuser(@Body() body: CreateUserDto) {
-    console.log(body);
+    const { email, password } = body;
+    this.userService.create(email, password);
   }
 }
