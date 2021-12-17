b4ca0dcc4e5e5e916e253ec82d01c01284c44216
Abdelhameed Ismail <3b7ameed@gmail.com>
2021-12-18 01:43:15 +0200

    User CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -10,5 +20,26 @@ export class UsersController {
   createuser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     this.userService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get('/')
+  getByEmail(@Query('email') email: string) {
+    console.log(email);
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,31 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    return user;
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    return this.repo.remove(user);
+  }
 }
