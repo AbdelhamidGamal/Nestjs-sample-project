2929d23f4104ac54c55d5dc35264fa5533962e83
Abdelhameed Ismail <3b7ameed@gmail.com>
2021-12-22 16:33:15 +0200

    AuthService testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,17 +1,29 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
   let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    // Create a fake copy of the users service
+    const users: User[] = [];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -27,27 +39,49 @@ describe('AuthService', () => {
     service = module.get(AuthService);
   });
 
-  it('Can create an istance of AuthService', async () => {
+  it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
 
   it('creates a new user with a salted and hashed password', async () => {
-    const user = await service.signup('3b7ameed@gmail.com', 'Anything');
+    const user = await service.signup('asdf@asdf.com', 'asdf');
 
-    expect(user.email).toEqual('3b7ameed@gmail.com');
-    expect(user.password).not.toEqual('Anything');
-    expect(user.password).toContain('.');
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 
-  // Something wrong with new version of jest
-  //   it('throws an error if user signs up with email that is in use', async (done) => {
-  //     fakeUsersService.find = () =>
-  //       Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
-  //     service
-  //       .signup('asdf@asdf.com', 'asdf')
-  //       .then((x) => x)
-  //       .catch((e) => {
-  //         done();
-  //       });
-  //   });
+  it('throws an error if user signs up with email that is in use', async (done) => {
+    await service.signup('asdf@asdf.com', 'asdf');
+    try {
+      await service.signup('asdf@asdf.com', 'asdf');
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('throws if signin is called with an unused email', async (done) => {
+    try {
+      await service.signin('asdflkj@asdlfkj.com', 'passdflkj');
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('throws if an invalid password is provided', async (done) => {
+    await service.signup('laskdjf@alskdfj.com', 'password');
+    try {
+      await service.signin('laskdjf@alskdfj.com', 'laksdlfkj');
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    await service.signup('asdf@asdf.com', 'mypassword');
+
+    const user = await service.signin('asdf@asdf.com', 'mypassword');
+    expect(user).toBeDefined();
+  });
 });
